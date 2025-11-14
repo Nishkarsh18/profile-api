3e603c6b061fdb6590443f52343d013fa967e724
Nishkarsh <nishkarshbisht18@gmail.com>
2025-11-14 14:07:34 +0530

    new file added

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """test api view"""
+
+    def get(self,request,format=None):
+        """Reture lsit of apiview featurs"""
+        an_apiview =[
+            'uses',
+            'django view similar',
+            'more control'
+        ]
+        return Response({'message':'hell','an_apiview':an_apiview})
